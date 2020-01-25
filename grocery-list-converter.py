30f784f40fda507d9a76d28a373fd6d7ec4d1aba
NateKistler <natekistler@gmail.com>
2020-01-25 18:55:12 -0500

    Initial Commit

---
+++ grocery-list-converter.py
@@ -0,0 +1,29 @@
+produce_list = ['green pepper',
+                'red pepper',
+                'yellow pepper',
+                'white onion',
+                'yellow onion',
+                'green onion',
+                'brussel sprouts',
+                'green beans',
+                'avocados']
+
+fruit_list = ['strawberries',
+              'blueberries',
+              'raspberries',
+              'black berries',
+              'bananas',
+              'kiwis',
+              'tomatoes',
+              'apples',
+              ]
+
+dairy_list = []
+
+meat_dept_list = []
+
+baking_list = []
+
+beverage_list = []
+
+products_dict = {'Produce': produce_list}
