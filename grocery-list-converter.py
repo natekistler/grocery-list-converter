c2a06ba86f49dca4a836edf8f5664af32d6432ff
NateKistler <natekistler@gmail.com>
2020-01-26 13:29:28 -0500

    Initial Commit

--- grocery-list-converter.py
+++ grocery-list-converter.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 grocery_store_inventory = pd.read_excel('grocery-store-items.xlsx')
 
+for cols in grocery_store_inventory.columns:
+    try:
+        grocery_store_inventory[cols] = grocery_store_inventory[cols].str.lower()
+    except:
+        pass
+
 grocery_list = []
 
-for item in grocery_store_inventory['Item']:
-    print(item)
+for item in grocery_list:
+    for inventory in grocery_store_inventory['list']:
+        print(item)
