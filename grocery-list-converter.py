185ba6d546d7ed7b6326209638d856265d1208d9
NateKistler <natekistler@gmail.com>
2020-01-27 12:33:28 -0500

    Initial Commit

--- grocery-list-converter.py
+++ grocery-list-converter.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 grocery_store_inventory = pd.read_excel('grocery-store-items.xlsx')
 
+def unique(lis):
+    unique_list = []
+    for i in lis:
+        if i not in unique_list:
+            unique_list.append(i)
+    return unique_list
+
 #Make all items and locations in grocey_store_inventory lowercase to ease string comparison
 for cols in grocery_store_inventory.columns:
     try:
@@ -9,8 +16,30 @@ for cols in grocery_store_inventory.columns:
     except:
         pass
 
-grocery_list = []
+grocery_list = ['Milk', 'Eggs', 'Apples', 'Strawberries']
+grocery_list_df = pd.DataFrame(grocery_list, columns=['List_Item'])
+grocery_list_df['List_Item'] = grocery_list_df['List_Item'].str.lower()
 
-for item in grocery_list:
-    for inventory in grocery_store_inventory['list']:
-        print(item)
+locations_list = unique(grocery_store_inventory['Location'])
+
+location_dic = {}
+
+for location in locations_list:
+    location_dic.update({location: []})
+
+location_dic.update({'other': []})
+
+len_list = len(grocery_list)
+count = 1
+for item in grocery_list_df['List_Item']:
+    try:
+        i = grocery_store_inventory.loc[grocery_store_inventory['Item'] == item]
+        location_dic[grocery_store_inventory['Location'][i.index[0]]].append(item)
+        print('Added', item, ':',  count, 'of', len_list, 'to shopping list')
+        count += 1
+    except (ValueError, IndexError):
+        location_dic['other'].append(item)
+        print('Added', item, ":", count, 'of', len_list, 'to shopping list')
+        count += 1
+
+print(location_dic)
